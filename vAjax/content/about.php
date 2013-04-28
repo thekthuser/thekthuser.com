76c67df910c08473e6e3b1d59b30c4f038b3ea9f
thekthuser <thekthuser@thekthuser.com>
2013-04-28 15:50:59 -0400

    switching away form ajax loading, archived vAjax folder

---
+++ vAjax/content/about.php
@@ -0,0 +1,18 @@
+<div class="content">
+<h2>What is k<sup>th</sup>?</h2>
+<p>Pronoucned "kayth"; a specific, yet arbitrary number.<br />
+1 .. k .. n</p>
+
+</div>
+
+<div class="content">
+<h2>Why is k<sup>th</sup> monospaced?</h2>
+<p>The color scheme is based on my coding environment. The font is different, but Inconsolata is a better looking font for this purpose. The Reddit Picture Frame, although hosted here, doesn't match the rest of the site because it's meant to stand on it's own.</p>
+</div>
+
+
+<div class="content">
+<h2>Who is k<sup>th</sup>?</h2>
+<p>I am a developer living in the NY/Tri-state area. Python is often my language of choice, but lately I have been using PHP and Javascript more often. At home I run openSUSE with KDE. In the past I have used other Linux distros as well as Mac and Windows. I like "jailbreaking" my devices are eventually I do that to everything I own.</p>
+<p>If you want to contact me fill out the form <a href='#'>here</a>.</p>
+</div>
---
+++ vAjax/content/sample_code.php
@@ -0,0 +1,34 @@
+<div class="content">
+    <h2>Blocks Puzzle</h2>
+    <p>A fun puzzle someone sent me. I wrote the solution in Python. Given a sequence of blocks of random length, arrange them into columns without changing their order such that the sum of the differences between each column length and the average length of all columns (the balance delta) is minimized. A zip containing source code and sample input are provided. Input must be formatted as the sample is - with one block's size per line.</p>
+    <div class="link">
+        ~ <a href="downloads/blocks.zip">zip</a>
+        ~ <a href="images/blocks_example.jpg">example image</a>
+    </div>
+</div>
+
+<div class="content">
+    <h2>TI-BASIC</h2>
+    <p>The following programs are some of the first I ever wrote. I got a TI-83 Plus graphing calculator for math classes in high school and when I realized I could make games on it I set about doing that. They were lost for many years until I dicovered them backed up onto a cd in the attic. The source code was still lost for a few years following that because they were set to be non-editable. I was finally able to recover the source with the help of some TI assembly utilities. If you want to run them you will need a TI-83 Plus emulator and ROM in addition to the linked 8XP files. A popular emulator is linked here.</p>
+    <div class="link">
+        ~ <a href="http://lpg.ticalc.org/prj_tilem/index.html">link</a>
+    </div>
+</div>
+
+<div class="content">
+    <h2>FORMULAS</h2>
+    <p>I cut my teeth automating formulas as they were introduced during math classes. This program is a collection of several of those. There are ASDFASDF listed and they are mostly Trigonometry and Pre-Calculus level mathematics.</p>
+    <div class="link">~ <a href="downloads/FORMULAS.zip">zip</a></div>
+</div>
+
+<div class="content">
+    <h2>MAROONED</h2>
+    <p>MAROONED is the first game I wrote. The player is stuck on a deserted island and needs to survive until a plane can be signaled. The game can be finished in about 5 minutes, but is highly dependent on luck.</p>
+    <div class="link">~ <a href="downloads/MAROONED.zip">zip</a></div>
+</div>
+
+<div class="content">
+    <h2>DIABLO</h2>
+    <p>Blizzard's Diablo was a favorite game of mine and this is a text-only version of that for the TI-83+. All 3 character classes are available, can gain experience, and equip different weapons. There are many monsters from the original game included and most, if not all, bosses can be randomly encountered. If you want to play, make sure to check the README. There is a little preparation that needs to be done before starting the game if you intend to save.</p>
+    <div class="link">~ <a href="DIABLO.zip">zip</a></div>
+</div>
