cbae48b2e7d3432ee6608b901bc4576a9038912b
thekthuser <thekthuser@thekthuser.com>
2014-03-29 15:28:51 -0400

    put tracked events on downloads and ajax mode toggle

--- content/ajax_mode.php
+++ content/ajax_mode.php
@@ -3,8 +3,10 @@
     <p>This site was originally written with Ajax, and later rewritten in HTML for more desciptive urls. You can toggle between both modes by clicking the button below.</p>
     <br />
     <?php if ($_COOKIE['mode'] == 'ajax') { ?>
-    <button type="button" id="ajax_toggle">Switch to HTML</button>
+    <button type="button" id="ajax_toggle" 
+    onClick="ga('send', 'event', 'Toggle', 'toggle_mode', 'to HTML');">Switch to HTML</button>
     <?php } else { ?>
-    <button type="button" id="ajax_toggle">Switch to Ajax</button>
+    <button type="button" id="ajax_toggle"
+    onClick="ga('send', 'event', 'Toggle', 'toggle_mode', 'to Ajax');">Switch to Ajax</button>
     <?php } ?>
 </div>
--- content/sample_code.php
+++ content/sample_code.php
@@ -2,7 +2,7 @@
     <h2>Blocks Puzzle</h2>
     <p>A fun puzzle someone sent me. I wrote the solution in Python. Given a sequence of blocks of random length, arrange them into columns without changing their order such that the sum of the differences between each column length and the average length of all columns (the balance delta) is minimized. A zip containing source code and sample input are provided. Input must be formatted as the sample is - with one block's size per line.</p>
     <div class="link">
-        ~ <a href="downloads/blocks.zip">zip</a>
+        ~ <a href="downloads/blocks.zip" onClick="ga('send', 'event', 'Downloads', 'blocks');">zip</a>
         ~ <span onclick="$('.image').toggle()">example image</span>
     </div>
         <img class="image" src="images/blocks_example.jpg"
@@ -20,19 +20,19 @@
 <div class="content">
     <h2>FORMULAS</h2>
     <p>I cut my teeth automating formulas as they were introduced during math classes. This program is a collection of several of those. There are 9 listed and they are mostly Trigonometry and Pre-Calculus level mathematics.</p>
-    <div class="link">~ <a href="downloads/FORMULAS.zip">zip</a></div>
+    <div class="link">~ <a href="downloads/FORMULAS.zip" onClick="ga('send', 'event', 'Downloads', 'formulas');">zip</a></div>
 </div>
 
 <div class="content">
     <h2>MAROONED</h2>
     <p>MAROONED is the first game I wrote. The player is stuck on a deserted island and needs to survive until a plane can be signaled. The game can be finished in about 5 minutes, but is highly dependent on luck.</p>
-    <div class="link">~ <a href="downloads/MAROONED.zip">zip</a></div>
+    <div class="link">~ <a href="downloads/MAROONED.zip" onClick="ga('send', 'event', 'Downloads', 'marooned');">zip</a></div>
 </div>
 
 <div class="content">
     <h2>DIABLO</h2>
     <p>Blizzard's Diablo was a favorite game of mine and this is a text-only version of that for the TI-83+. All 3 character classes are available, can gain experience, and equip different weapons. There are many monsters from the original game included and most, if not all, bosses can be randomly encountered. If you want to play, make sure to check the README. There is a little preparation that needs to be done before starting the game if you intend to save.</p>
-    <div class="link">~ <a href="DIABLO.zip">zip</a></div>
+    <div class="link">~ <a href="DIABLO.zip" onClick="ga('send', 'event', 'Downloads', 'diablo');">zip</a></div>
 </div>
 
 <div class="bottom"></div>
