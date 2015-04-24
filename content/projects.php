99d7fc5c31a3f719cb083fb862336c015f4d88ab
thekthuser <thekthuser@thekthuser.com>
2015-04-24 18:04:42 -0400

    updated projects

--- content/projects.php
+++ content/projects.php
@@ -1,4 +1,10 @@
 <div class="content">
+<h2>Approachable Art</h2>
+<p>An art gallery and store split into two sites. The gallery is finished. The store is not. These are my first and second Django projects, respectively. Artwork can be displayed in three sizes: thumbnails for browsing, screen-size, and full-size to see fine detail. An admin dashboard allows artists to add/edit/remove works and biographical information.</p>
+<div class="link">~ <a href="http://www.dennisgreenapproachableart.com">gallery</a> ~ store</div>
+</div>
+
+<div class="content">
 <h2>Second Saturday</h2>
 <p>Second Saturday Staten Island is a monthly art walk on Staten Island, NY. The site was static originally. After recovering the domain and web hosting from its previous owner I wrote the backend in PHP with a MySQL database. The new admin console allows the site maintainers to post updates without mucking about in HTML. A dynamic Google Map was also added to show the locations of sponsors, members, and events.</p>
 <div class="link">~ <a href="http://secondsaturdaystatenisland.org">link</a></div>
@@ -20,21 +26,28 @@
 </div>
 
 <div class="content">
+<h2>Hiroko Creative Educator</h2>
+<p>This is a very simple site that was recreated pixel for pixel mostly to get the owner off of a web host that was ripping her off.</p>
+<div class="link">~ <a href="http://www.hirokocreativeeducator.com">link</a></div>
+</div>
+
+<div class="content">
 <h2>Socially Awkward Magazine</h2>
-<p>Socially Awkward Magazine is a Wordpress site that the owner asked me for help on. I created a custom page for displaying artists who blog on the site. I also tidied up his original install by adding and removing appropriate plugins and adjusting his configuration to eliminate any security issues, etc. There is more work to be done, but the owner is not yet ready.</p>
+<p>Socially Awkward Magazine is a Wordpress site that the owner asked me for help on. I created a custom page for displaying artists who blog on the site. I also tidied up his original install by adding and removing appropriate plugins and adjusting his configuration to eliminate any security issues, etc.</p>
+<p>Note: This site is no longer live.</p>
 <div class="link">~ <a href="http://sociallyamag.com/">link</a></div>
 </div>
 
 <div class="content">
 <h2>The Skull Society</h2>
 <p>This was a weekend project for a friend. It was created in phpBB and customized to his requests.</p>
-<p>Note: The site is no longer live. The owner neglected to reregister the domain.</p>
+<p>Note: The site is no longer live.</p>
 <div class="link">~ <a href="http://the-skull-society.net/">link</a></div>
 </div>
 
 <div class="content">
 <h2>TrickleUp</h2>
-<p>This is an ongoing project. TrickleUp was one of several non-profits who participated in the Helping Hacks hackathon in August 2012. A team of originally five people worked together to improve TrickleUp's internal data collection and visualization system. We received the "best technical solution" award. Progress is stalled waiting for more input from TrickleUp.</p>
+<p>TrickleUp was one of several non-profits who participated in the Helping Hacks hackathon in August 2012. A team of originally five people worked together to improve TrickleUp's internal data collection and visualization system. We received the "best technical solution" award. Progress is stalled waiting for more input from TrickleUp.</p>
 <div class="link">~ <a href="http://trickleup.org/">link</a></div>
 </div>
 <div class="bottom"></div>
